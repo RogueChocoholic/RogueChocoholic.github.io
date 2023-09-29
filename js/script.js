725b2b8c6b501132eedecd162b6df4e05b138031
RogueChocoholic <139319195+RogueChocoholic@users.noreply.github.com>
2023-09-29 17:53:41 +0530

    projectfolio Page created

---
+++ js/script.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+  $("#welcomeSub").hide();
+  $("#contentsContainer").hide();
+  $("#page2Begin").hide();
+  $("#downButton1").hide();
+
+  $(document).mousemove(function () {
+    $("#myName").animate(
+      {
+        marginLeft: "52px",
+      },
+      600,
+      function () {}
+    );
+
+    $("#welcome").animate(
+      {
+        marginLeft: "50px",
+      },
+      1000,
+      function () {}
+    );
+
+    $("#welcomeName").animate(
+      {
+        marginLeft: "50px",
+      },
+      850,
+      function () {}
+    );
+
+    $("#welcomeName2").animate(
+      {
+        marginLeft: "50px",
+      },
+      1000,
+      function () {}
+    );
+
+    $("#welcomeSub").fadeIn(1500);
+    $("#downButton1").fadeIn(1500);
+
+    $("#contentsContainer").fadeIn(2000);
+
+    $("#goToPageOne").click(function() {
+      $("#page2Begin").fadeIn();
+    });
+
+    $("#downButton1").click(function() {
+      $("#page2Begin").fadeIn(500);
+    });
+
+
+
+
+
+
+
+
+  });
+
+
+
+
+
+});
+
+
+function say_Size() {
+  var w = window.innerWidth;
+
+  alert(w);
+  
+}
+
+
